b0fd60a92fb164dbb151e139f1dd6734e790974e
Ximelz <80809203+Ximelz@users.noreply.github.com>
2025-12-15 16:09:08 +0300

    HW 2 send to review

---
+++ Homework 2/DirectoryReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace Homework_2
+{
+    public class DirectoryReader
+    {
+        public async Task GetSpacesFromFilesAsyncInDirecrory(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            int result = 0;
+            List<Task<int>> tasks = new List<Task<int>>();
+            FileReader fileReader = new FileReader();
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine($"Количество файлов в папке \"{path}\" = {filePaths.Length}");
+
+            stopwatch.Start();
+
+            foreach (string filePath in filePaths)
+                tasks.Add(Task.Run(() => fileReader.GetSpacesAsyncFromFile(filePath)));
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+                result += task.Result;
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Количество пробелов во всех файлах = {result}.\r\n" +
+                              $"Общее время выполнения = {stopwatch.Elapsed}");
+        }
+    }
+}
---
+++ Homework 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework_2
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string dirPath = "";
+
+            List<Task> tasks = new List<Task>();
+
+            DirectoryReader dirRead = new DirectoryReader();
+
+            tasks.Add(Task.Run(() => dirRead.GetSpacesFromFilesAsyncInDirecrory(dirPath)));
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
